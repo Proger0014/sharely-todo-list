2fd01bba7332ec6f1488fdb3cb05c75261b496ef
proger0014 <iakov56@yandex.ru>
2023-04-13 22:06:14 +0600

    Добавил комментарий для WithoutPassword утилы

--- SharelyTodoList/Utils/HeadersUtils.cs
+++ SharelyTodoList/Utils/HeadersUtils.cs
@@ -2,6 +2,12 @@
 
 public static class HeadersUtils
 {
+    /// <summary>
+    /// Необходим для фронтенда. Если пароль на группу не ставили,
+    /// то при переходе на группу по ссылке, получая данный заголовок,
+    /// не будет необходимости вводить пароль, а сразу откроет страницу
+    /// </summary>
+    /// <param name="headers"></param>
     public static void WithoutPassword(IHeaderDictionary headers)
     {
         headers.Add("without-password", "1");
