21841bed30b7e2a7a2c9fd741b31cdf99754ff1e
proger0014 <iakov56@yandex.ru>
2023-04-13 21:55:30 +0600

    Фикс последнего коммита

---
+++ SharelyTodoList/Interfaces/Repositories/IGroupRepository.cs
@@ -0,0 +1,9 @@
+﻿using SharelyTodoList.Models.Group;
+
+namespace SharelyTodoList.Interfaces.Repositories;
+
+public interface IGroupRepository
+{
+    Group? GetById(long id);
+    long Create(Group newGroup);
+}
---
+++ SharelyTodoList/Interfaces/Services/IGroupService.cs
@@ -0,0 +1,9 @@
+﻿using SharelyTodoList.Models.Group;
+
+namespace SharelyTodoList.Interfaces.Services;
+
+public interface IGroupService
+{
+    Group GetById(long id);
+    long CreateGroup(string name, string password);
+}
