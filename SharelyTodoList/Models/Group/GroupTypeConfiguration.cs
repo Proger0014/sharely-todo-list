13767f5c11df05c7c1e1cb7b98e79cd4d7b7f6c3
proger0014 <iakov56@yandex.ru>
2023-04-16 23:17:25 +0600

    добавил ограничения на максимальную длину символов строчных свойств модели Group

--- SharelyTodoList/Models/Group/GroupTypeConfiguration.cs
+++ SharelyTodoList/Models/Group/GroupTypeConfiguration.cs
@@ -8,5 +8,11 @@ public class GroupTypeConfiguration : IEntityTypeConfiguration<Group>
     public void Configure(EntityTypeBuilder<Group> builder)
     {
         builder.HasKey(g => g.Id);
+
+        builder.Property(g => g.Name)
+            .HasMaxLength(200);
+
+        builder.Property(g => g.Password)
+            .HasMaxLength(50);
     }
 }
